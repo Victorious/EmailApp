e7f6709b37f335dcb39bb3d5bb10048a53abc0c9
Marcus Wilen <marcus.wilen@hotmail.com>
2019-06-02 18:16:40 +0200

    Adding Admin View

--- EmailApp/src/Controllers/AdminController.java
+++ EmailApp/src/Controllers/AdminController.java
@@ -1,7 +1,28 @@
 package Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class AdminController {
 
-
+//	FXML Variables
+	@FXML TextField firstnameTextField;
+	@FXML TextField lastnameTextField;
+	@FXML TextField usernameTextField;
+	@FXML TextField emailTextField;
+	@FXML TextField passwordTextField;
+	@FXML Button updateUserBtn;
+	@FXML Button updateUserPasswordBtn;
+	
+//	Variables
+	
+	public void UpdateUserData() {
+		
+	}
+	
+	public void UpdateUserPassword() {
+		
+	}
 	
 }
